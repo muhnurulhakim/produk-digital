f2c9ec4348925a1f719a4cde36594d964271e208
muhnurulhakim <116804209+muhnurulhakim@users.noreply.github.com>
2024-05-09 06:08:05 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,22 @@
 const loginForm = document.getElementById('login-form');
 
 loginForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = this.email.value;
-    const password = this.password.value;
+  event.preventDefault();
+  const email = this.email.value;
+  const password = this.password.value;
 
-    // Check login credentials
-    if (email === 'muina@duck.com' && password === '007') {
-        // Simpan status login di penyimpanan sesi
-        sessionStorage.setItem('isLoggedIn', 'true');
+  // Check login credentials
+  if (email === 'muina@duck.com' && password === '007') {
+    // Simpan status login di penyimpanan sesi
+    sessionStorage.setItem('isLoggedIn', 'true');
 
-        // Redirect ke halaman admin dashboard
-        window.location.href = 'admin-dashboard.html';
-    } else {
-        alert('Email atau password salah. Silakan coba lagi.');
-    }
+    // Prompt pengguna untuk memasukkan token akses GitHub
+    var githubAccessToken = prompt('Masukkan Token Akses GitHub Anda:');
+    setGitHubAccessToken(githubAccessToken);
+
+    // Redirect ke halaman admin dashboard
+    window.location.href = 'admin-dashboard.html';
+  } else {
+    alert('Email atau password salah. Silakan coba lagi.');
+  }
 });
